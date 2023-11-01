47d8baa6b743919ba50656b8cbebbef98f00651c
Pomierski <pomierskipiotr@gmail.com>
2023-11-01 18:06:44 +0100

    rename

--- src/poc/getReleaseHeaderOnly.ts
+++ src/poc/getReleaseHeaderOnly.ts
@@ -2,8 +2,8 @@ import { readFile } from "fs/promises";
 import { getReleaseVersion } from "../core/getReleaseVersion";
 import { getRequiredChangelogChunks } from "../core/getRequiredChangelogChunks";
 
-export const getReleaseHeaderOnly = async (configPath: string) => {
-  const data = await readFile(configPath, "utf8");
+export const getReleaseHeaderOnly = async (changelogPath: string) => {
+  const data = await readFile(changelogPath, "utf8");
 
   const requiredChangelogChunks = await getRequiredChangelogChunks(data);
   const releaseVersion = getReleaseVersion(requiredChangelogChunks);
